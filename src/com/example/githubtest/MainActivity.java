5aea79463b0a6755c7fe2d9fd5e97f4c2af1197d
WindFengzi <2424966967@qq.com>
2016-08-03 11:00:25 +0800

    update MainActivity and add png_3.png by hzx date 2016/8/3

--- src/com/example/githubtest/MainActivity.java
+++ src/com/example/githubtest/MainActivity.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView tv;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		tv = (TextView) findViewById(R.id.tv_hello);
+		tv.setText("hello kitty");
 	}
 
 	@Override
